2db13fc30c292cd18fbe8cce251217b95b9967d1
Bradley Marques <bradleyrcmarques@gmail.com>
2022-05-04 20:45:02 +0200

    Use .env file

--- catswipe/settings.py
+++ catswipe/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import environ
 from pathlib import Path
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-5*l%5+r@*i9fasjezbqwvvxin259=3-%7w1()9b1@ae_y%rh4a"
+SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env("DJANGO_DEBUG") == "True"
 
 ALLOWED_HOSTS = []
 
